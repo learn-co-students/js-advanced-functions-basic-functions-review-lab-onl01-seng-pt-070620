b4fddb7a8bc71d997e9b0876534f8dcfa17b1b53
julieconnors <juliana.g.connors@gmail.com>
2021-01-29 06:09:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+let wrapAdjective = function(param="*") {
+    return function(adj="special") {
+        return `You are ${param}${adj}${param}!`;
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+
+    subtract: function(num1, num2) {
+        return num1 - num2;
+    },
+
+    multiply: function(num1, num2) {
+        return num1 * num2;
+    },
+
+    divide: function(num1, num2) {
+        return num1 / num2;
+    }
+}
+
+// function actionApplyer(int, numArray) {
+//     if (numArray === []) {
+//         return int;
+//     } else {
+//         let multiplied = Calculator.multiply(int, numArray[0]);
+//         let added = Calculator.add(multiplied, numArray[1]);
+//         return added % numArray[2]; 
+//     }
+// }
+
+let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
